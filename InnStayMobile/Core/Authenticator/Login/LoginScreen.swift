6233c6cbc6b085ea2f14145aab2e9f6a8dac9e3d
Rares Carbunaru <rarescarbunaru@Raress-MacBook-Air-2.local>
2025-05-19 23:57:18 +0300

    Added Login & SignUp Requests via API

--- InnStayMobile/Core/Authenticator/Login/LoginScreen.swift
+++ InnStayMobile/Core/Authenticator/Login/LoginScreen.swift
@@ -67,7 +67,7 @@ struct LoginScreen: View {
             // Login Button
             Button {
                 print("Log in tapped")
-                onLoginSuccess()
+                performLogin()
             } label: {
                 Text("Log In")
                     .fontWeight(.bold)
@@ -102,7 +102,52 @@ struct LoginScreen: View {
         .padding(.horizontal, 32)
         .background(Color.white)
     }
+    
+    
+    private func performLogin() {
+          guard let url = URL(string: "http://127.0.0.1:5000/api/auth/login") else {
+              print("Invalid URL")
+              return
+          }
+
+          let body: [String: String] = [
+              "email": email,
+              "password": password
+          ]
+
+          var request = URLRequest(url: url)
+          request.httpMethod = "POST"
+          request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+          do {
+              request.httpBody = try JSONEncoder().encode(body)
+          } catch {
+              print("Encoding error:", error)
+              return
+          }
+
+          URLSession.shared.dataTask(with: request) { data, response, error in
+              if let error = error {
+                  print("Login error:", error)
+                  return
+              }
+
+              guard let httpResponse = response as? HTTPURLResponse else { return }
+
+              if httpResponse.statusCode == 200 {
+                  DispatchQueue.main.async {
+                      onLoginSuccess()
+                  }
+              } else {
+                  print("Login failed with status code:", httpResponse.statusCode)
+              }
+          }.resume()
+      }
+    
+    
 }
+
+
 
 #Preview {
     LoginScreen(
--- InnStayMobile/Core/Authenticator/SignUp/SignUpScreen.swift
+++ InnStayMobile/Core/Authenticator/SignUp/SignUpScreen.swift
@@ -15,7 +15,7 @@ struct SignupScreen: View {
     
     @State private var firstName = ""
     @State private var lastName = ""
-    @State private var age = ""
+    @State private var username = ""
     @State private var phoneNumber = ""
     @State private var email = ""
     @State private var password = ""
@@ -42,7 +42,7 @@ struct SignupScreen: View {
                 Group {
                     CustomTextField(title: "First Name", text: $firstName)
                     CustomTextField(title: "Last Name", text: $lastName)
-                    CustomTextField(title: "Username", text: $age, keyboardType: .numberPad)
+                    CustomTextField(title: "Username", text: $username, keyboardType: .numberPad)
                     CustomTextField(title: "Phone Number", text: $phoneNumber, keyboardType: .phonePad)
                     CustomTextField(title: "Email", text: $email, keyboardType: .emailAddress)
                     
@@ -67,7 +67,7 @@ struct SignupScreen: View {
                 
                 // Sign Up Button
                 Button {
-                    onSignupSuccess()
+                    performSignup(onSuccess: onSignupSuccess)
                 } label: {
                     Text("Sign Up")
                         .fontWeight(.bold)
@@ -93,6 +93,57 @@ struct SignupScreen: View {
         }
         .background(Color.white)
     }
+    
+    func performSignup(onSuccess: @escaping () -> Void) {
+        guard let url = URL(string: "http://127.0.0.1:5000/api/auth/register") else {
+            print("Invalid signup URL")
+            return
+        }
+
+        let body: [String: String] = [
+            "first_name": firstName,
+            "last_name": lastName,
+            "username": username,
+            "phone_number": phoneNumber,
+            "email": email,
+            "password": password,
+            "role": "user"
+        ]
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        do {
+            request.httpBody = try JSONEncoder().encode(body)
+        } catch {
+            print("Encoding error:", error)
+            return
+        }
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Signup error:", error)
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("No HTTP response")
+                return
+            }
+
+            print("Signup response: \(httpResponse.statusCode)")
+
+            if httpResponse.statusCode == 201 || httpResponse.statusCode == 200 {
+                DispatchQueue.main.async {
+                    onSuccess()
+                }
+            } else {
+                print("Signup failed with code:", httpResponse.statusCode)
+            }
+        }.resume()
+    }
+    
 }
 
 struct CustomTextField: View {
